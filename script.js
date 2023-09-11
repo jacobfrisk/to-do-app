bf9b63a08c9ce654b3eb865519d6250579d4f272
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-09-11 14:29:12 +0200

    Initial code deleted

---
+++ script.js
@@ -0,0 +1,16 @@
+// Funktion der opretter opgave
+
+// Opgaven får et random ID med Math.random
+// Opgave får tilegnet en streng
+
+// Opgaven tilføjes til en "To-Do" liste
+
+// Opgaven kan få en dato
+
+// Opgave sorteres efter dato
+
+// Opgaven flyttes til en "Vigtig" liste hvis den er vigtig
+
+// Opgave kan flyttes til en "Færdig" liste
+
+// Opgave kan slettes
