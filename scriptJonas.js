f62cba414e80de8562b9dee8191fdcde40a4ce09
JonasBlaedel <119848473+JonasBlaedel@users.noreply.github.com>
2023-09-11 15:07:43 +0200

    datafield id

--- scriptJonas.js
+++ scriptJonas.js
@@ -1,18 +1,24 @@
-const toDo = [];
+// Funktion der opretter opgave i et tomt array.
+let toDo = [];
 const submit = document.querySelector("button");
 const input = document.querySelector("input");
+const randomNum = Math.floor(Math.random() * 100) + 1;
 
 submit.addEventListener("click", () => {
   console.log("her er vi");
   const assign = {
+    // Strengen der skrives ud er det samme som det input der laves
     desc: input.value,
+    // Et tilfældigt ID tilføjes med Math.random
+    id: Math.floor(Math.random() * 5000) + 1,
   };
 
   toDo.push(assign);
 
   displayList();
-  console.log(toDo);
 });
+
+console.log(toDo);
 
 function displayList(assignments = toDo) {
   const tbody = document.querySelector("#list tbody");
@@ -32,8 +38,23 @@ function displayAssignment(string) {
   const clone = document.querySelector("template#toDos").content.cloneNode(true);
 
   // set clone data
-  clone.querySelector("[data-field=toDo]").textContent = string.desc;
-
+  clone.querySelector("[data-field=toDo]").textContent = `Description: ${string.desc}`;
+  clone.querySelector(".toDoObject").setAttribute("data-field", string.id);
   // append clone to list
   document.querySelector("#list tbody").appendChild(clone);
 }
+
+// Opgaven får et random ID med Math.random
+// Opgave får tilegnet en streng
+
+// Opgaven tilføjes til en "To-Do" liste
+
+// Opgaven kan få en dato
+
+// Opgave sorteres efter dato
+
+// Opgaven flyttes til en "Vigtig" liste hvis den er vigtig
+
+// Opgave kan flyttes til en "Færdig" liste
+
+// Opgave kan slettes
